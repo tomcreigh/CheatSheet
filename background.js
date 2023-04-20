b4b043914ba87414941174d681dd5b914afb94ca
tomc <tmcgraphic@gmail.com>
2023-04-20 11:38:14 +0100

    eleventh commit - console logging cleared - contextMenus removed

--- background.js
+++ background.js
@@ -1,38 +1,56 @@
 function restoreOptions() {
-    let font = "clear";
-    chrome.storage.sync.get({
-        font: true,
-        remove_distractions: true,
-        restructure_text: true,
-        display_num_text: true,
-        enable_sticky_tabs: true
-    }, function(options) {
-        chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-            chrome.tabs.sendMessage(tabs[0].id, { type: 'pass', message: 'font_' + font });
+  let font = "clear";
+  chrome.storage.sync.get(
+    {
+      font: true,
+      remove_distractions: true,
+      restructure_text: true,
+      display_num_text: true,
+      enable_sticky_tabs: true,
+    },
+    function (options) {
+      chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        chrome.tabs.sendMessage(tabs[0].id, {
+          type: "pass",
+          message: "font_" + font,
         });
-        chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-            chrome.tabs.sendMessage(tabs[0].id, { type: 'pass', message: 'remove_distractions_' + options.remove_distractions });
+      });
+      chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        chrome.tabs.sendMessage(tabs[0].id, {
+          type: "pass",
+          message: "remove_distractions_" + options.remove_distractions,
         });
-        chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-            chrome.tabs.sendMessage(tabs[0].id, { type: 'pass', message: 'restructure_text_' + options.restructure_text });
+      });
+      chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        chrome.tabs.sendMessage(tabs[0].id, {
+          type: "pass",
+          message: "restructure_text_" + options.restructure_text,
         });
-        chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-            chrome.tabs.sendMessage(tabs[0].id, { type: 'pass', message: 'display_num_text_' + options.display_num_text });
+      });
+      chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        chrome.tabs.sendMessage(tabs[0].id, {
+          type: "pass",
+          message: "display_num_text_" + options.display_num_text,
         });
-        chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-            chrome.tabs.sendMessage(tabs[0].id, { type: 'pass', message: 'enable_sticky_tabs_' + options.enable_sticky_tabs });
+      });
+      chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        chrome.tabs.sendMessage(tabs[0].id, {
+          type: "pass",
+          message: "enable_sticky_tabs_" + options.enable_sticky_tabs,
         });
-    });
+      });
+    }
+  );
 }
 
 chrome.runtime.onMessage.addListener((data) => {
-    if (data.type === "pass") {
-        chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-            chrome.tabs.sendMessage(tabs[0].id, { message: data.message });
-        });
-    }
-    if (data.type === "DCL") {
-        console.log("recieved")
-        restoreOptions();
-    }
+  if (data.type === "pass") {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+      chrome.tabs.sendMessage(tabs[0].id, { message: data.message });
+    });
+  }
+  if (data.type === "DCL") {
+    // console.log("recieved");
+    restoreOptions();
+  }
 });
--- js/popup.js
+++ js/popup.js
@@ -5,12 +5,12 @@ const current_font = document.getElementById("current_font");
 let font;
 
 const remove_distractions = document.getElementById(
-    "remove_distractions_checkbox"
+  "remove_distractions_checkbox"
 );
 const restructure_text = document.getElementById("restructure_text_checkbox");
 const display_num_text = document.getElementById("display_num_text_checkbox");
 const enable_sticky_tabs = document.getElementById(
-    "enable_sticky_tabs_checkbox"
+  "enable_sticky_tabs_checkbox"
 );
 const settings = document.getElementById("actions");
 
@@ -21,113 +21,112 @@ const settings = document.getElementById("actions");
 // ************************************************************** Enable Sticky Tabs
 
 function saveOptions() {
-    chrome.storage.sync.set({
-            font: font,
-            remove_distractions: remove_distractions.checked,
-            restructure_text: restructure_text.checked,
-            display_num_text: display_num_text.checked,
-            enable_sticky_tabs: enable_sticky_tabs.checked,
-        },
-        function() {
-            chrome.runtime.sendMessage("", { type: "pass", message: "font_" + font });
-            chrome.runtime.sendMessage("", {
-                type: "pass",
-                message: "remove_distractions_" + remove_distractions.checked,
-            });
-            chrome.runtime.sendMessage("", {
-                type: "pass",
-                message: "restructure_text_" + restructure_text.checked,
-            });
-            chrome.runtime.sendMessage("", {
-                type: "pass",
-                message: "display_num_text_" + display_num_text.checked,
-            });
-            chrome.runtime.sendMessage("", {
-                type: "pass",
-                message: "enable_sticky_tabs_" + enable_sticky_tabs.checked,
-            });
-        }
-    );
+  chrome.storage.sync.set(
+    {
+      font: font,
+      remove_distractions: remove_distractions.checked,
+      restructure_text: restructure_text.checked,
+      display_num_text: display_num_text.checked,
+      enable_sticky_tabs: enable_sticky_tabs.checked,
+    },
+    function () {
+      chrome.runtime.sendMessage("", { type: "pass", message: "font_" + font });
+      chrome.runtime.sendMessage("", {
+        type: "pass",
+        message: "remove_distractions_" + remove_distractions.checked,
+      });
+      chrome.runtime.sendMessage("", {
+        type: "pass",
+        message: "restructure_text_" + restructure_text.checked,
+      });
+      chrome.runtime.sendMessage("", {
+        type: "pass",
+        message: "display_num_text_" + display_num_text.checked,
+      });
+      chrome.runtime.sendMessage("", {
+        type: "pass",
+        message: "enable_sticky_tabs_" + enable_sticky_tabs.checked,
+      });
+    }
+  );
 }
 
 function restoreOptions() {
-    chrome.storage.sync.get({
-            font: true,
-            remove_distractions: true,
-            restructure_text: true,
-            display_num_text: true,
-            enable_sticky_tabs: true,
-        },
-        function(options) {
-            font = font;
-            remove_distractions.checked = options.remove_distractions;
-            restructure_text.checked = options.restructure_text;
-            display_num_text.checked = options.display_num_text;
-            enable_sticky_tabs.checked = options.enable_sticky_tabs;
-        }
-    );
+  chrome.storage.sync.get(
+    {
+      font: true,
+      remove_distractions: true,
+      restructure_text: true,
+      display_num_text: true,
+      enable_sticky_tabs: true,
+    },
+    function (options) {
+      font = font;
+      remove_distractions.checked = options.remove_distractions;
+      restructure_text.checked = options.restructure_text;
+      display_num_text.checked = options.display_num_text;
+      enable_sticky_tabs.checked = options.enable_sticky_tabs;
+    }
+  );
 }
 document.addEventListener("DOMContentLoaded", restoreOptions);
 
 function closeDropDown() {
-    font_dropdown.classList.remove("show");
-    font_button.classList.remove("selected");
-    font_arrow.classList.remove("rotate");
+  font_dropdown.classList.remove("show");
+  font_button.classList.remove("selected");
+  font_arrow.classList.remove("rotate");
 }
 
 function displayCurrentFont(font) {
-
-    if (font === 'clear') {
-        current_font.textContent = font;
-        current_font.classList.remove("show");
-        console.log(font)
-    } else {
-        current_font.textContent = font;
-        current_font.classList.add("show");
-    }
+  if (font === "clear") {
+    current_font.textContent = font;
+    current_font.classList.remove("show");
+    // console.log(font);
+  } else {
+    current_font.textContent = font;
+    current_font.classList.add("show");
+  }
 }
 
 function set_font(f) {
-    font = f;
-    displayCurrentFont(font);
-    closeDropDown();
+  font = f;
+  displayCurrentFont(font);
+  closeDropDown();
 }
 
 public_sans.addEventListener("click", () => {
-    set_font("public_sans");
-    saveOptions();
+  set_font("public_sans");
+  saveOptions();
 });
 
 roboto.addEventListener("click", () => {
-    set_font("roboto");
-    saveOptions();
+  set_font("roboto");
+  saveOptions();
 });
 
 clear.addEventListener("click", () => {
-    set_font("clear");
-    saveOptions();
+  set_font("clear");
+  saveOptions();
 });
 
 var checkbox_nodes = document.getElementsByClassName("toggle-checkbox");
 var i;
 for (i = 0; i < checkbox_nodes.length; i++) {
-    checkbox_nodes[i].addEventListener("click", saveOptions);
+  checkbox_nodes[i].addEventListener("click", saveOptions);
 }
 
 // ************************************************************** settings
 settings.addEventListener("click", () => {
-    chrome.runtime.sendMessage("", {
-        type: "pass",
-        message: "settings",
-    });
-    if (chrome.runtime.openOptionsPage) {
-        chrome.runtime.openOptionsPage();
-    } else {
-        window.open(chrome.runtime.getURL('options.html'));
-    }
+  // chrome.runtime.sendMessage("", {
+  //     type: "pass",
+  //     message: "settings",
+  // });
+  if (chrome.runtime.openOptionsPage) {
+    chrome.runtime.openOptionsPage();
+  } else {
+    window.open(chrome.runtime.getURL("options.html"));
+  }
 });
-
-
 
 // ****************************************************************************************************************************
 // ************************************************************** Font Dropdown Menu
@@ -136,13 +135,13 @@ const font_dropdown = document.getElementById("font_dropdown");
 const font_arrow = document.getElementById("font_arrow");
 
 font_button.addEventListener("click", () => {
-    if (!font_dropdown.classList.contains("show")) {
-        font_dropdown.classList.toggle("show");
-        font_button.classList.toggle("selected");
-        font_arrow.classList.toggle("rotate");
-    } else {
-        font_dropdown.classList.toggle("show");
-        font_button.classList.toggle("selected");
-        font_arrow.classList.toggle("rotate");
-    }
+  if (!font_dropdown.classList.contains("show")) {
+    font_dropdown.classList.toggle("show");
+    font_button.classList.toggle("selected");
+    font_arrow.classList.toggle("rotate");
+  } else {
+    font_dropdown.classList.toggle("show");
+    font_button.classList.toggle("selected");
+    font_arrow.classList.toggle("rotate");
+  }
 });
